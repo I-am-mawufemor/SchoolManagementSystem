b7a8641ac71f2c3b3f8df7c59ca39e17c1154872
I-am-mawufemor <norbert.mawufemor@gmail.com>
2025-08-18 19:38:10 +0000

    Assessment grading

---
+++ AssessmentGrading.java
@@ -0,0 +1,126 @@
+public enum AssessmentGrading {
+    Excellent(1),
+    VeryGood(2),
+    Good(3),
+    HighAverage(4),
+    Average(5),
+    LowAverage(6),
+    Poor(7),
+    Fail(8);
+
+    private final int value;
+
+    AssessmentGrading(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public class Assessment {
+    private String subject;
+    private double project;
+    private double groupWork;
+    private double testOne;
+    private double testTwo;
+    private double examScore;
+
+    public Assessment() {}
+
+    public Assessment(String subject, double project, double groupWork, double testOne, double testTwo, double examScore) {
+        this.subject = subject;
+        this.project = project;
+        this.groupWork = groupWork;
+        this.testOne = testOne;
+        this.testTwo = testTwo;
+        this.examScore = examScore;
+    }
+
+    // Setters
+    public void setSubject(String subject) { 
+        this.subject = subject; 
+    }
+    public void setProject(double project) { 
+        this.project = project; 
+    }
+    public void setGroupWork(double groupWork) { 
+        this.groupWork = groupWork; 
+    }
+    public void setTestOne(double testOne) { 
+        this.testOne = testOne; 
+    }
+    public void setTestTwo(double testTwo) { 
+        this.testTwo = testTwo; 
+    }
+    public void setExamsScore(double examScore) { 
+        this.examScore = examScore;
+     }
+
+    // Getters
+    public String getSubject() { 
+        return subject; 
+    }
+    public double getProject() { 
+        return project; 
+    }
+    public double getGroupWork() { 
+        return groupWork; 
+    }
+    public double getTestOne() { 
+        return testOne; 
+    }
+    public double getTestTwo() { 
+        return testTwo; 
+    }
+    public double getExamsScore() { 
+        return examScore; 
+    }
+
+    public double totalClassScore() {
+        return groupWork + project + testOne + testTwo;
+    }
+
+    public double classScore50percent(){
+        return totalClassScore() * 0.5;
+    }
+
+    public double totalExamsScore() {
+        return examScore;
+    }
+
+    public double examsScore50percent() {
+        return totalExamsScore() * 0.5;
+    }
+
+    public double total() {
+        return totalClassScore() + totalExamsScore();
+    }
+
+    public AssessmentGrading getGrade() {
+        double score = total();
+        if (score >= 90) return AssessmentGrading.Excellent;
+        else if (score >= 80) return AssessmentGrading.VeryGood;
+        else if (score >= 70) return AssessmentGrading.Good;
+        else if (score >= 60) return AssessmentGrading.HighAverage;
+        else if (score >= 50) return AssessmentGrading.Average;
+        else if (score >= 40) return AssessmentGrading.LowAverage;
+        else if (score >= 30) return AssessmentGrading.Poor;
+        else return AssessmentGrading.Fail;
+    }
+
+    public String toString() {
+        return "Subject: " + subject + "\n" +
+               "Project: " + project + "\n" +
+               "Group Work: " + groupWork + "\n" +
+               "Test One: " + testOne + "\n" +
+               "Test Two: " + testTwo + "\n" +
+               "Exam Score: " + examScore + "\n" +
+               "Total Class Score: " + totalClassScore() + "\n" +
+               "Total Exams Score: " + totalExamsScore() + "\n" +
+               "Overall Total Score: " + total() + "\n" +
+               "Grade: " + getGrade();
+    }
+}
+
+}
